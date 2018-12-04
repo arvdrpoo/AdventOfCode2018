f2bab4ece7391727f27cf2ef1345078ac095e08f
VAN DER POORTEN Arnaud <avanderpoorten@alvey.be>
2018-12-04 21:36:17 +0100

    day 4 WIP

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -1,12 +1,39 @@
 ﻿open System
 open Util.Regex
 
-type GuardSchedule() =
-    [<DefaultValue>] val mutable Id : int
-    [<DefaultValue>] val mutable AsleepTimesByDay : Map<int*int,bool []> // tuple of (month,day) to array (length 60) of minutes guard is asleep
-
 let split (input:string)=
     input.Split("\r\n")
+
+let rec handle guard (datastructure:Map<string,Map<string,bool []>>) arr =
+    match arr with
+    | head :: tail ->
+        match head with
+        | Regex @"\[.+?-(\d\d\-\d\d) \d+:(\d+)\] (.+)" [day; min; action] ->
+            match action with
+            | Regex @"Guard #(\d+)" [newguard] ->
+                if datastructure.ContainsKey newguard then
+                    handle newguard datastructure tail
+                else
+                    // create new level in datastructure
+                    // new array of waking minutes, default to sleep
+                    let minutes : bool array = Array.zeroCreate 60
+
+                    // guard starts out as awake
+                    minutes.[min|>int] <- true
+                    let newMap =
+                        Map.empty
+                        |> Map.add day minutes
+                    let newDs =
+                        datastructure
+                        |> Map.add newguard newMap
+                    handle newguard newDs tail
+            | Regex @"wakes up" [] ->
+
+            | Regex @"falls asleep" []->
+
+            | _ -> handle guard datastructure tail
+        | _ ->
+    | [] -> datastructure
 
 [<EntryPoint>]
 let main argv =
@@ -28,19 +55,11 @@ let main argv =
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up"
 
-    let guards = List.empty
+    // (guard,day,minute) -> awake
 
-    let gs = new GuardSchedule()
-    gs.Id <- 0
+    let split = split testinput
 
-    for line in (split testinput) do
-        match line with
-        | Regex @"\[\d+-(\d+)-(\d+) \d+:(\d+)\] (.+)" [month; day; minute; action;] ->
-            match action with
-            | Regex @".+#(\d).+" [id] ->
-                if gs.Id <>0 then
-
-            |
-        | _ -> printfn "error parsing line %s" line
+    split
+    |>
 
     0 // return an integer exit code
